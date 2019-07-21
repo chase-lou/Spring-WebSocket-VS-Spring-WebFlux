eae23624715728f4130a021e374db6c5993bf0f7
chase-lou <ccl1616@126.com>
2019-07-21 15:34:33 +0800

    test

--- websocket-spring-webflux/src/main/java/com/chase/demo/websocket/spring/webflux/config/LettuceClientConfigurationFactory.java
+++ websocket-spring-webflux/src/main/java/com/chase/demo/websocket/spring/webflux/config/LettuceClientConfigurationFactory.java
@@ -2,20 +2,36 @@ package com.chase.demo.websocket.spring.webflux.config;
 
 import java.time.Duration;
 
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.resource.ClientResources;
+import io.lettuce.core.resource.DefaultClientResources;
 import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
+import org.springframework.stereotype.Component;
 
 public class LettuceClientConfigurationFactory implements FactoryBean<LettuceClientConfiguration> {
+
+    @Autowired
+    private ClientResources clientResources;
+
     @Override
-    public LettuceClientConfiguration getObject() throws Exception {
+    public LettuceClientConfiguration getObject() {
         GenericObjectPoolConfig poolConfig = new GenericObjectPoolConfig();
         poolConfig.setMaxTotal(200);
         poolConfig.setMaxWaitMillis(2000);
         poolConfig.setTestOnBorrow(true);
-        LettuceClientConfiguration lettuceClientConfiguration = LettucePoolingClientConfiguration.builder()
-                .poolConfig(poolConfig).commandTimeout(Duration.ofMillis(3000)).shutdownTimeout(Duration.ZERO).build();
+        LettuceClientConfiguration lettuceClientConfiguration =
+                LettucePoolingClientConfiguration
+                        .builder()
+                        .poolConfig(poolConfig)
+                        .commandTimeout(Duration.ofMillis(3000))
+                        .shutdownTimeout(Duration.ZERO)
+                        .clientResources(clientResources)
+                        .build();
 
         return lettuceClientConfiguration;
     }
